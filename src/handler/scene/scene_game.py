426dfbb49926d78ce02a780a05ea36fb43dc2b8b
zurdi <zurdizurdo25@gmail.com>
2018-01-30 18:37:14 +0100

    scenes

--- src/handler/scene/scene_game.py
+++ src/handler/scene/scene_game.py
@@ -9,7 +9,6 @@ import config
 # ---------------------------------------------------------------------
 
 class SceneGame(scene.Scene):
-    """Escena inicial del juego, esta es la primera que se carga cuando inicia"""
 
     def __init__(self, director):
         scene.Scene.__init__(self, director)
